f2edeb0dd2dd1a3a7fb0c36735df09e849c40101
junohpark221 <junohpark221@gmail.com>
2024-04-02 00:50:24 +0900

    refactor: Car 도메인 클래스 내의 이름 길이 검증 로직 추가

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -3,15 +3,24 @@ package racing.domain;
 import racing.generator.NumberGenerator;
 
 public class Car {
+    private static final int THRESHOLD = 3;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
     private final String carName;
     private final NumberGenerator numberGenerator;
     private int position;
-    private final int THRESHOLD = 3;
 
     public Car(String carName, NumberGenerator numberGenerator) {
+        validateCarName(carName);
         this.carName = carName;
         this.numberGenerator = numberGenerator;
         this.position = 0;
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw new RuntimeException();
+        }
     }
 
     public int proceed() {
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -7,12 +7,13 @@ import racing.generator.FalseNumberGenerator;
 import racing.generator.TrueNumberGenerator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarTest {
     @DisplayName("4~9까지의 숫자가 나올 경우 전진함을 확인하는 테스트")
     @Test
     void go() {
-        Car car = new Car("car_name", new TrueNumberGenerator());
+        Car car = new Car("name", new TrueNumberGenerator());
 
         car.proceed();
 
@@ -22,10 +23,18 @@ public class CarTest {
     @DisplayName("0~3 사이의 숫자가 나올 경우 전진하지 않음을 확인하는 테스트")
     @Test
     void stop() {
-        Car car = new Car("car_name", new FalseNumberGenerator());
+        Car car = new Car("name", new FalseNumberGenerator());
 
         car.proceed();
 
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @DisplayName("5글자 이상의 CarName일 경우 Car가 생성되지 않음을 확인하는 테스트")
+    @Test
+    void validateName() {
+        assertThrows(RuntimeException.class, () -> {
+            new Car("car_name", new FalseNumberGenerator());
+        });
     }
 }
